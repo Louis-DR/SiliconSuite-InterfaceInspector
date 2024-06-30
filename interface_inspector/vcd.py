f9c76180081ac1fc39c1332a21a393eba1485c02
Louis Duret-Robert <louisduret@gmail.com>
2024-06-30 22:13:17 +0200

    [VCD] Replace string value with VCD value class

--- interface_inspector/vcd.py
+++ interface_inspector/vcd.py
@@ -3,25 +3,43 @@ from bisect import bisect_right
 from pyDigitalWaveTools.vcd.parser import VcdParser, VcdVarScope
 
 class ValueFormat(Enum):
-  BINARY      = 0
-  OCTAL       = 1
-  DECIMAL     = 2
-  HEXADECIMAL = 3
+  BINARY      =  2
+  OCTAL       =  8
+  DECIMAL     = 10
+  HEXADECIMAL = 16
 
 class VCDValue:
-  def __init__(self, value, format:ValueFormat):
+  def __init__(self, value:str|int):
     if isinstance(value,int):
-      self.value = value
+      self.raw_value  = value
+      self.raw_format = None
+      self.value      = value
+      self.has_xz     = False
     elif isinstance(value,str):
-      match format:
-        case ValueFormat.BINARY:
-          self.value = int(value,2)
-        case ValueFormat.OCTAL:
-          self.value = int(value,8)
-        case ValueFormat.DECIMAL:
-          self.value = int(value,10)
-        case ValueFormat.HEXADECIMAL:
-          self.value = int(value,16)
+      self.raw_value = value
+      value_no_xz    = value.replace('x','0').replace('X','0').replace('z','0').replace('Z','0')
+      self.has_xz    = value == value_no_xz
+      if len(value) == 1:
+        self.value     = int(value)
+        self.raw_value = ValueFormat.BINARY
+      else:
+        identifier_code = value[0]
+        value           = value[1:]
+        value_no_xz     = value_no_xz[1:]
+        match identifier_code:
+          case "b" | "B":
+            self.value     = int(value,2)
+            self.raw_value = ValueFormat.BINARY
+          case "r" | "R":
+            self.value     = int(value,10)
+            self.raw_value = ValueFormat.DECIMAL
+  def bin_display(self) -> str:
+    if self.raw_format == ValueFormat.BINARY:
+      return self.raw_value
+    else:
+      return bin(self.value)[2:]
+  def bin_index(self) -> str:
+    return self.bin_display()[::-1]
   def __index__(self):
     return self.value
   def __eq__(self, value: object) -> bool:
@@ -38,12 +56,12 @@ class VCDValue:
     return self.value <  value
 
 class VCDSample:
-  def __init__(self,timestamp:int,value:str):
+  def __init__(self,timestamp:int,value:VCDValue):
     self.timestamp = timestamp
     self.value = value
 
   @classmethod
-  def from_tuple(classs,tuple:tuple[int,str]):
+  def from_tuple(classs,tuple:tuple[int,VCDValue]):
     return classs(tuple[0],tuple[1])
 
   def __tuple__(self):
@@ -95,7 +113,7 @@ class VCDSignal:
             self.finished = True
         return None
       search_sample = self.vcd[search_index]
-      if search_sample.value == "1":
+      if search_sample.value == 1:
         if move:
           self.current_index  = search_index
           self.current_sample = search_sample
@@ -104,8 +122,8 @@ class VCDSignal:
   def get_edge_at_timestamp(self, timestamp:int, polarity:EdgePolarity=EdgePolarity.RISING, direction:TimeDirection=TimeDirection.NEXT, exclusive:bool=False, move:bool=False) -> VCDSample:
     search_sample = self.get_at_timestamp(timestamp, move=move)
     if not (    ( search_sample.timestamp == timestamp )
-            and (  ( polarity == EdgePolarity.RISING  and search_sample.value == "1" )
-                or ( polarity == EdgePolarity.FALLING and search_sample.value == "0" ) ) ):
+            and (  ( polarity == EdgePolarity.RISING  and search_sample.value == 1 )
+                or ( polarity == EdgePolarity.FALLING and search_sample.value == 0 ) ) ):
       search_sample = self.get_edge(polarity, direction, move=move)
     return search_sample
 
@@ -116,10 +134,17 @@ class VCDFile:
       vcd.parse(vcd_file)
     self.vcd = vcd.scope
 
-  def get_signal(self, path:list[str], scope:VcdVarScope=None):
+  def get_signal(self, path:list[str], scope:VcdVarScope=None) -> VCDSignal:
     if scope is None:
       scope = self.vcd
     if not path:
-      return VCDSignal([VCDSample(vcd_tuple[0],vcd_tuple[1]) for vcd_tuple in scope.data])
+      vcd_samples = []
+      for sample_tuple in scope.data:
+        sample_timestamp = sample_tuple[0]
+        sample_value     = VCDValue(sample_tuple[1])
+        vcd_sample       = VCDSample(sample_timestamp, sample_value)
+        vcd_samples.append(vcd_sample)
+      vcd_signal = VCDSignal(vcd_samples)
+      return vcd_signal
     else:
       return self.get_signal(path[1:], scope.children[path[0]])
